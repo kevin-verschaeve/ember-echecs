ce4c616582a44bc810ada5870c5bf4bc6a39bb4a
Nicolas Beze <nicolas.beze@gmail.com>
2014-11-11 18:20:40 +0100

    calcul des deplacements possible

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,5 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	actions: {
+		additionMatrice: function(x, y, pion){
+			var deplacementPossible = [];
+			for(i = 0; i < 15; i++) {
+  				for(j = 0; j < 15; j++) {
+  					deplacementPossible[i][j] = pion.matrice[i][j] + plateau[i+x][j+y]
+  				}
+  			}
+  			return deplacementPossible;
+		}
 	}
 });
