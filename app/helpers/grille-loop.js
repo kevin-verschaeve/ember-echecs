70370d4da9fc0d76ae2090bddf4d138f1cd3c9f9
kevin-verschaeve <kevin.verschaeve@live.fr>
2014-11-11 17:36:35 +0100

    creation tableau avec pions

--- app/helpers/grille-loop.js
+++ app/helpers/grille-loop.js
@@ -1,17 +1,29 @@
 import Ember from 'ember';
 
-export default Ember.Handlebars.makeBoundHelper(function(input) {
+export default Ember.Handlebars.makeBoundHelper(function(model) {
   	var grille = '',
   		i, j,
-  		cell_class = 'odd'
+  		cell_class = 'odd',
+  		pions = [
+  			['&#9820;', '&#9822;', '&#9821;', '&#9819;', '&#9818;', '&#9821;', '&#9822;', '&#9820;'],
+  			['&#9823;', '&#9823;', '&#9823;', '&#9823;', '&#9823;', '&#9823;', '&#9823;', '&#9823;'],
+  			['', '', '', '', '', '', '', ''],
+  			['', '', '', '', '', '', '', ''],
+  			['', '', '', '', '', '', '', ''],
+  			['', '', '', '', '', '', '', ''],
+  			['&#9817;', '&#9817;', '&#9817;', '&#9817;', '&#9817;', '&#9817;', '&#9817;', '&#9817;'],
+  			['&#9814;', '&#9816;', '&#9815;', '&#9813;', '&#9812;', '&#9815;', '&#9816;', '&#9814;']
+  		]
   	;
 
-
   	grille += '<table>';
-  	for(i = 1; i <= 8; i++) {
+  	for(i = 0; i < 8; i++) {
   		grille += '<tr>';
-  		for(j = 1; j <= 8; j++) {
-  			grille += '<td class="'+cell_class+'"></td>';
+  		for(j = 0; j < 8; j++) {
+  			// console.log(pions[i][j]);
+  			grille += '<td class="'+cell_class+'">';
+  			grille += '<a href="#" class="js-pion">'+pions[i][j]+'</a>';
+  			grille += '</td>';
   			cell_class = changeClass(cell_class);
   		}
   		cell_class = changeClass(cell_class);
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,68 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function() {
-
+		return [
+			Ember.Object.create({
+				id: 1,
+				nom: 'pion',
+				image: '&#9817;'
+			}),
+			Ember.Object.create({
+		  		id: 2,
+		  		nom: 'tour',
+		  		image: '&#9814;'
+		  	}),
+		  	Ember.Object.create({
+		  		id: 3,
+		  		nom: 'cavalier',
+		  		image: '&#9816;'
+		  	}),
+		  	Ember.Object.create({
+		  		id: 4,
+		  		nom: 'fou',
+		  		image: '&#9815;'
+		  	}),
+		  	Ember.Object.create({
+		  		id: 5,
+		  		nom: 'roi',
+		  		image: '&#9812;'
+		  	}),
+		  	Ember.Object.create({
+		  		id: 6,
+		  		nom: 'reine',
+		  		image: '&#9813;'
+		  	}),
+		  	Ember.Object.create({
+				id: 7,
+				nom: 'pion',
+				image: '&#9823;',
+			}),
+			Ember.Object.create({
+		  		id: 8,
+		  		nom: 'tour',
+		  		image: '&#9820;'
+		  	}),
+		  	Ember.Object.create({
+		  		id: 9,
+		  		nom: 'cavalier',
+		  		image: '&#9822;'
+		  	}),
+		  	Ember.Object.create({
+		  		id: 10,
+		  		nom: 'fou',
+		  		image: '&#9821;'
+		  	}),
+		  	Ember.Object.create({
+		  		id: 11,
+		  		nom: 'roi',
+		  		image: '&#9818;'
+		  	}),
+		  	Ember.Object.create({
+		  		id: 12,
+		  		nom: 'reine',
+		  		image: '&#9819;'
+		  	})
+		];
 	}
 });
+
