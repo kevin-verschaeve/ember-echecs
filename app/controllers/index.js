56836d59539eb89f98a43149c989f42d2026cd82
Nicolas Beze <nicolas.beze@gmail.com>
2014-11-11 19:35:24 +0100

    modif

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,9 +4,12 @@ export default Ember.Controller.extend({
 	actions: {
 		additionMatrice: function(x, y, pion){
 			var deplacementPossible = [];
-			for(i = 0; i < 15; i++) {
-  				for(j = 0; j < 15; j++) {
-  					deplacementPossible[i][j] = pion.matrice[i][j] + plateau[i+x][j+y]
+			for(var i = 0; i < 15; i++) {
+  				for(var j = 0; j < 15; j++) {
+  					if((x + i) > 7 && (y + j) > 7 && (x + i) < 16 && (y + j) < 16){
+  						deplacementPossible[x + i - 7][y + j - 7] = pion.matrice[i][j] + plateau[i+x][j+y]
+  					}
+  					
   				}
   			}
   			return deplacementPossible;
