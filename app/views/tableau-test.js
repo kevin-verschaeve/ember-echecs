a13e844f4e4fd37c708ce558b731290ff68975ee
kevin-verschaeve <kevin.verschaeve@live.fr>
2014-11-16 18:43:11 +0100

    tout dans la vue

--- app/views/tableau-test.js
+++ app/views/tableau-test.js
@@ -2,29 +2,203 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
 	templateName: 'tableau-test',
+	isSelected: false,
+	cases_accessibles: [],
+	couleur_pion: '',
 	pion: null,
 	old: null,
 	click: function(e) {
-		var clicked = e.target,
-			td_parent = $(e.target).parent('td'),
-			cases_accessibles = this.get('controller.cases_accessibles'),
-			plateau = this.get('controller.model.plateau');
+		// if(this.isSelected == false) {
+			this.set('isSelected', true);
 
-		if($(clicked).hasClass('pion') && cases_accessibles.indexOf('#'+td_parent.attr('id')) == -1) {
-			this.set('pion', $(clicked));
-			this.set('old', td_parent);
-		}
+			var clicked = e.target,
+				td_parent = $(e.target).parent('td'),
+				pions = this.get('controller.model.pions'),
+				plateau = this.get('controller.model.plateau'),
+				i,
+				deplacements = [],
+				id = $(clicked).data('id_pion'),
+				x = td_parent.data('x'),
+				y = td_parent.data('y');
 
-		if(td_parent.hasClass('selected')) {
-			plateau = deplacePion(clicked, this.pion, this.old, plateau, this.get('controller.couleur_pion'));
-			this.set('controller.model.plateau', plateau);
+			for(i = 0; i < pions.length; i++) {
+				if(pions[i].id === id) {
+					deplacements = additionMatrice(x, y, pions[i], plateau);
+					this.couleur_pion = pions[i].id < 7 ? 'B' : 'N';
+					break;
+				}
+			}
 
-			this.get('controller').send('reset');
-			this.set('pion', null);
-			this.set('old', null);
-		}
+			// resetSelected();
+			var t = '<table>';
+			for(var j = 0; j < deplacements.length; j++) {
+				t += '<tr>';
+				for(var k = 0; k < deplacements.length; k++) {
+					t += '<td>'+deplacements[j][k]+'</td>';
+					if(deplacements[j][k] == 0) {
+						this.cases_accessibles.push('#case_'+k+'_'+j);
+						$('#case_'+k+'_'+j).addClass('selected');
+					}
+				}
+				t += '</tr>';
+			}
+			t += '</table>';
+
+			$('.deuxieme').html(t);
+
+
+			if($(clicked).hasClass('pion') && this.cases_accessibles.indexOf('#'+td_parent.attr('id')) == -1) {
+				this.set('pion', $(clicked));
+				this.set('old', td_parent);
+				this.cases_accessibles = [];
+			}
+
+			if(td_parent.hasClass('selected')) {
+				plateau = deplacePion(clicked, this.pion, this.old, plateau, this.couleur_pion);
+				this.set('controller.model.plateau', plateau);
+
+				// this.get('controller').send('reset');
+				resetSelected();
+				this.set('isSelected', false);
+				this.set('pion', null);
+				this.set('old', null);
+			}
+
+		// } else {
+		// 	this.set('isSelected', false);
+		// 	resetSelected();
+		// }
 	}
 });
+
+function resetSelected() {
+	$('td').removeClass('selected');
+}
+
+function additionMatrice(x, y, pion, plateau) {
+	var deplacementPossible = [
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0]
+	],
+	i, j;
+
+	if(pion.id < 7){
+		var couleur = "B";
+	}else{
+		var couleur = "N";
+	}
+
+	for(i = 0; i < 8; i++) {
+		for(j = 0; j < 8; j++) {
+
+			var valeur = 0;
+
+			if((plateau[j][i] == "N" && couleur == "N") || (plateau[j][i] == "B" && couleur == "B")){
+				valeur = 3; //Mes pions
+				pion.matrice[7 - x + i][7 - y + j];
+			}
+			if((plateau[j][i] == "B" && couleur == "N") || (plateau[j][i] == "N" && couleur == "B")){
+				if(pion.matrice[7 - x + i][7 - y + j] == 0 || pion.matrice[7 - x + i][7 - y + j] == 2){
+					valeur = 1; //pion adverse mais déplacement possible
+				}else{
+					valeur = 2; //déplacement impossible
+				}
+			}
+			if(plateau[j][i] == 0){
+				if(pion.matrice[7 - x + i][7 - y + j] == 0){
+					valeur = 0; //case vide et déplacement possible
+				}else{
+					valeur = 2; //déplacement impossible
+				}
+			}
+			deplacementPossible[j][i] =  valeur;
+		}
+	}
+
+
+	var fouTourOuReine = [0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+	if(fouTourOuReine[pion.id] == 1){
+		for(i = 0; i < 8; i++) {
+			for(j = 0; j < 8; j++) {
+				//si case possible pour le moment
+				if(deplacementPossible[j][i] == 0 || deplacementPossible[j][i] == 1){
+					deplacementPossible[j][i] = 0;
+					if(j-y == 0){
+						//cas vertical
+						if(i-x > 1 || x-i > 1){
+							if(i<x){
+								var depart = i + 1;
+								var fin = x - 1;
+							}else{
+								var depart = x + 1;
+								var fin = i - 1;
+							}
+							for(var z = depart; z < fin; z++){
+								if(deplacementPossible[j][z] != 0){
+									deplacementPossible[j][i] = 1;
+								}
+							}
+						}
+
+					}else if(i-x == 0){
+						//cas horizontal
+						if(j-y > 1 || y-j > 1){
+							if(j<y){
+								var depart = j + 1;
+								var fin = y - 1;
+							}else{
+								var depart = y + 1;
+								var fin = j - 1;
+							}
+
+							for(var z = depart; z <= fin; z++){
+								if(deplacementPossible[z][i] != 0){
+									deplacementPossible[j][i] = 1;
+								}
+							}
+
+						}
+
+					}
+					/*else{
+						//cas diagonal
+						if((i-x > 1 || x-i > 1) && (j-y > 1 || y-j > 1)){
+							if(i<x){
+								var depart = i + 1;
+								var fin = x - 1;
+							}else{
+								var depart = x + 1;
+								var fin = i - 1;
+							}
+							if(j<y){
+								var depart2 = j + 1;
+								var fin2 = y - 1;
+							}else{
+								var depart2 = y + 1;
+								var fin2 = j - 1;
+							}
+							for(var z = depart; z <= fin; z++){
+								for(var z2 = depart2; z2 <= fin2; z2++){
+									if(deplacementPossible[z][z2] != 0){
+										deplacementPossible[j][i] = 1;
+									}
+								}
+							}
+						}
+					}*/
+				}
+			}
+		}
+	}
+
+	return deplacementPossible;
+}
 
 function deplacePion(clicked, pion, old, plateau, couleur_pion) {
 	var x_old = old.data('x'),
